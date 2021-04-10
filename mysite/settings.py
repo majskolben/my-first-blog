4522c7f581a1e6edd204f547004995ab46c10054
maya <maya@mongers.org>
2021-04-10 15:18:36 +0200

    'ALLOWED_HOSTS'

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ud94synp-o6%e5@_zxe(nac)o&+s7hx9n7z03*s=dx$&)-gepf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'majskolben.pythonanywhere.com']
 
